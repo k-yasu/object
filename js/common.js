bd4408602eac4a8df60b33a5f5a16ef382c3edea
yasu kotaro <k-yasu@imjp.co.jp>
2016-03-13 13:10:16 +0900

    add feature-A

--- js/common.js
+++ js/common.js
@@ -5,3 +5,22 @@ if(typeof MYAPP === 'undefined') {
 */
 console.log(typeof MYAPP);
 var MYAPP = MYAPP || {};
+
+MYAPP.namespace = function(ns_string) {
+    var parts = ns_string.split('.');
+    var parent = MYAPP;
+    var i;
+
+    if(parent[0] === 'MYAPP') {
+        parts = parts.slice(1);
+    }
+
+    for(i = 0; i < parts.length; i += 1) {
+        if(typeof parent[parts[i]] === 'undefined') {
+            parent[parts[i]] = {};
+        }
+        parent = parent[parts[i]];
+    }
+
+    return parent;
+};
